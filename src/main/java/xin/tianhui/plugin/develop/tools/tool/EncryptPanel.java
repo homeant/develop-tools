bc2e38116eb6a5ea72211a57d923a104b7a55d73
homeant <homeanter@163.com>
2020-12-20 18:51:41 +0800

    加解密代码提交

--- src/main/java/xin/tianhui/plugin/develop/tools/tool/EncryptPanel.java
+++ src/main/java/xin/tianhui/plugin/develop/tools/tool/EncryptPanel.java
@@ -1,16 +1,86 @@
 package xin.tianhui.plugin.develop.tools.tool;
 
-import javax.swing.*;
-import java.awt.*;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import xin.tianhui.plugin.develop.tools.utils.AESUtils;
+import xin.tianhui.plugin.develop.tools.utils.DESUtils;
 
-public class EncryptPanel{
-    private JEditorPane editorPane1;
-    private JEditorPane editorPane2;
-    private JButton button1;
-    private JButton button2;
+import javax.swing.*;
+
+@Slf4j
+public class EncryptPanel {
     private JPanel mainPanel;
+    private JEditorPane plainText;
+    private JEditorPane cipherText;
+    private JButton encryptButton;
+    private JButton decodeButton;
+    private JRadioButton AESRadioButton;
+    private JRadioButton DESRadioButton;
+    private JTextField password;
+    private String encryptType;
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public EncryptPanel() {
+        radioAction(AESRadioButton);
+        radioAction(DESRadioButton);
+        encryptButton.addActionListener((e) -> {
+            String text = plainText.getText();
+            if (StringUtils.isNotBlank(text)) {
+                cipherText.setText(encrypt(encryptType, text));
+            }
+        });
+        decodeButton.addActionListener((e) -> {
+            String text = cipherText.getText();
+            if (StringUtils.isNotBlank(text)) {
+                plainText.setText(decode(encryptType, text));
+            }
+        });
+    }
+
+    public String encrypt(String type, String text) {
+        String encryptText = "";
+        try {
+            switch (type) {
+                case "AES":
+                    encryptText = AESUtils.encrypt(text, password.getText());
+                    break;
+                case "DES":
+                    encryptText = DESUtils.encrypt(text, password.getText());
+                    break;
+                default:
+
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return encryptText;
+    }
+
+    public String decode(String type, String text) {
+        String encryptText = "";
+        try {
+            switch (type) {
+                case "AES":
+                    encryptText = AESUtils.decode(text, password.getText());
+                    break;
+                case "DES":
+                    encryptText = DESUtils.decode(text, password.getText());
+                    break;
+                default:
+
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return encryptText;
+    }
+
+    public void radioAction(JRadioButton component) {
+        component.addActionListener((e) -> {
+            encryptType = component.getText();
+        });
     }
 }
